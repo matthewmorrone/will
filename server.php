6eece3458a784d1a8a124b41943473ad84765ec5
Matthew Morrone <matthewmorrone1@gmail.com>
2017-05-30 21:40:42 -0400

    implement super basic authorization

--- server.php
+++ server.php
@@ -17,22 +17,32 @@ if ($_POST) {
     // $database = database($database, $connect);
 	// $database 	= (isset($database) 	? $database 	: "");
 
-	$mysqli = new mysqli($address, $username, $password, $database);
 
 
 	switch($mode) {
 		case "all":
-			$query = 'SELECT id, created, title, blurb, image FROM posts ORDER BY created DESC';
+			$mysqli = connect($address, $username, $password, $database);
+			$query = 'SELECT id, created, title, blurb, image FROM posts ORDER BY created';
 			echo json_query($mysqli, $query);
 		break;
 		case "get":
+			$mysqli = connect($address, $username, $password, $database);
 			$id = (int)$id;
 			if (is_int($id)) {
 				$query = "SELECT id, created, modified, title, blurb, content, image FROM posts WHERE id = $id";
 				echo json_encode(get_query($mysqli, $query)[0][0]);
 			}
 		break;
+		case "auth":
+			if (strcmp($password, 'will') === 0) {
+				echo 'win';
+			}
+			else {
+				echo 'fail';
+			}
+		break;
 		case "new":
+			$mysqli = connect($address, $username, $password, $database);
 			$title = mysql_real_escape_string($title);
 			$blurb = mysql_real_escape_string($blurb);
 			$content = mysql_real_escape_string($content);
@@ -41,6 +51,7 @@ if ($_POST) {
 			// echo $mysqli->error."\n";
 		break;
 		case "edit":
+			$mysqli = connect($address, $username, $password, $database);
 			$title = mysql_real_escape_string($title);
 			$blurb = mysql_real_escape_string($blurb);
 			$content = mysql_real_escape_string($content);
@@ -49,6 +60,7 @@ if ($_POST) {
 			// echo $mysqli->error."\n";
 		break;
 		case "delete":
+			$mysqli = connect($address, $username, $password, $database);
 			$query = "DELETE FROM posts WHERE id = $id";
 			$result = $mysqli->query($query);
 			// echo $mysqli->error."\n";
@@ -60,11 +72,16 @@ if ($_POST) {
 
 
 
+	if (isset($mysqli)) {
+		mysqli_close($mysqli);
 
-	mysqli_close($mysqli);
+	}
 }
 
-
+function connect($address, $username, $password, $database) {
+	$mysqli = new mysqli($address, $username, $password, $database);
+	return $mysqli;
+}
 function pluck($array, $i = 0) {
 	$array2 = [];
 	foreach($array as $key=>$val) {
